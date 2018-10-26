9911ef76849d690da397257cf78b583ed9031792
elenaheaven <38784352+elenaheaven@users.noreply.github.com>
2018-10-26 18:22:08 +0300

    Almost ready

--- js/index.js
+++ js/index.js
@@ -1,25 +1,16 @@
-// Get the modal
-let modal = document.getElementById('modalBox');
+$(function() {
+    var $openModal = $("#openModal"),
+        $closeModal = $("#closeModal"),
+        $modal = $("#modalBox");
 
-// Get the button that opens the modal
-let a = document.getElementById('moreBtn');
+    $openModal.on('click', function() {
+    	$modal.show();
+      // $modal.addClass('modal-box-vis');
+    });
+    $closeModal.on('click', function() {
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+      $modal.fadeOut('slow');
 
-// When the user clicks the button, open the modal
-a.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+      // $modal.removeClass('modal-box-vis');
+    });
+});
